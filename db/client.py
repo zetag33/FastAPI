e7573d9eb10ac47b0bc6a3bc7a58c87be2036cd3
zetag33 <95145225+zetag33@users.noreply.github.com>
2023-11-23 11:46:31 +0100

    Create client.py

---
+++ db/client.py
@@ -0,0 +1,15 @@
+
+from pymongo import MongoClient
+
+# Elejir local o remoto
+
+# Base de datos local MongoDB
+db_client = MongoClient().local
+
+# Base de datos remota MongoDB Atlas (https://mongodb.com)
+# db_client = MongoClient(
+#     "mongodb+srv://<user>:<password>@<url>/?retryWrites=true&w=majority").test
+
+# Despliegue API en la nube:
+# Deta - https://www.deta.sh/
+# Intrucciones - https://fastapi.tiangolo.com/deployment/deta/
