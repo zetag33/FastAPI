111e3d5a1ef4ddb592dd1feb7025b5466173b364
zetag33 <95145225+zetag33@users.noreply.github.com>
2023-11-23 11:53:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ app.include_router(jwt_auth_users.router)
 
 app.include_router(users_db.router)
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
 async def root():
