152f4ed4a5ae789dae2e17db096ecf2c6d373c76
zetag33 <95145225+zetag33@users.noreply.github.com>
2023-11-24 14:16:01 +0100

    Update jwt_auth_users.py

--- routers/jwt_auth_users.py
+++ routers/jwt_auth_users.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Header
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import jwt, JWTError
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
-
+#pip install "python-jose[cryptography]"
+#pip install "passlib[bcrypt]"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_DURATION = 1
+ACCESS_TOKEN_DURATION = 3
 SECRET = "201d573bd7d1344d3a3bfce1550b69102fd11be3db6d379508b6cccc58ea230b"
 
 router = APIRouter(prefix="/jwtauth",
@@ -17,7 +18,7 @@ oauth2 = OAuth2PasswordBearer(tokenUrl="login")
 
 crypt = CryptContext(schemes=["bcrypt"])
 
-
+revoked_tokens = set()
 class User(BaseModel):
     username: str
     full_name: str
@@ -30,22 +31,24 @@ class UserDB(User):
 
 
 users_db = {
-    "mouredev": {
-        "username": "mouredev",
-        "full_name": "Brais Moure",
-        "email": "braismoure@mourede.com",
+    "Jerry": {
+        "username": "Jerry",
+        "full_name": "Jerry del Sauzal",
+        "email": "jerry.jerry.com",
         "disabled": False,
-        "password": "$2a$12$B2Gq.Dps1WYf2t57eiIKjO4DXC3IUMUXISJF62bSRiFfqMdOI2Xa6"
+        "password": "$2a$12$m/adJzFOUAM3VPYt8yRfnOR.Psz8W2piGit9/VVePmi5smaqYAkTy"
     },
-    "mouredev2": {
-        "username": "mouredev2",
-        "full_name": "Brais Moure 2",
-        "email": "braismoure2@mourede.com",
-        "disabled": True,
-        "password": "$2a$12$SduE7dE.i3/ygwd0Kol8bOFvEABaoOOlC8JsCSr6wpwB4zl5STU4S"
+    "Carmen": {
+        "username": "Carmen",
+        "full_name": "Carmen Sanchez",
+        "email": "carmen@gmail.com",
+        "disabled": False,
+        "password": "$2a$12$XHQSN8PsDjluRJYU/gZsUuRDqY5LxbFCppLlrSyA.9RXg6YkzjLTa"
     }
 }
 
+exception = HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
 
 def search_user_db(username: str):
     if username in users_db:
@@ -63,11 +66,24 @@ async def auth_user(token: str = Depends(oauth2)):
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Credenciales de autenticación inválidas",
         headers={"WWW-Authenticate": "Bearer"})
+    exception_invalidated = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Has modificado los datos del usuario, log in again",
+        headers={"WWW-Authenticate": "Bearer"})
+    exception_deleted = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="El usuario ya no existe",
+        headers={"WWW-Authenticate": "Bearer"})
 
     try:
         username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
         if username is None:
             raise exception
+        if token in revoked_tokens:
+            raise exception_invalidated
+        if username not in users_db:
+            raise exception_deleted
+
 
     except JWTError:
         raise exception
@@ -82,6 +98,16 @@ async def current_user(user: User = Depends(auth_user)):
             detail="Usuario inactivo")
 
     return user
+
+async def get_token(authorization: str = Header(None, convert_underscores=True)):
+    if not authorization or not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authorization header",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    token = authorization.split("Bearer ")[1]
+    return token
 
 
 @router.post("/login")
@@ -107,3 +133,43 @@ async def login(form: OAuth2PasswordRequestForm = Depends()):
 @router.get("/users/me")
 async def me(user: User = Depends(current_user)):
     return user
+
+@router.get("/users/all")
+async def get_users(user: User = Depends(current_user)):
+    return users_db
+
+@router.post("/create_user")
+async def me(user: UserDB):
+    if type(search_user(user.username)) == User:
+        raise Exception
+    password = crypt.hash(user.password)
+    user.password = password
+    name = user.username
+    users_db[user.username] = user.model_dump()
+    returned_user = User(**users_db[user.username])
+    return returned_user
+
+@router.delete("/delete_user")
+async def delete(user: User = Depends(current_user)):
+    if type(search_user(user.username)) != User:
+        raise Exception
+    name = user.username
+    del users_db[name]
+    return {"Success": "Deleted user succesfully"}
+
+@router.put("/update_user")
+async def update_user(updated_user: UserDB, current_user: User = Depends(current_user), token: str = Depends(get_token)):
+    if current_user.username != updated_user.username:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="No tienes permisos para actualizar este usuario")
+    password = crypt.hash(updated_user.password)
+    updated_user.password = password
+
+    del users_db[updated_user.username]
+    users_db[updated_user.username] = updated_user.model_dump()
+    revoked_tokens.add(token)
+
+    # Return the updated user information and the new token
+    return {"user": User(**users_db[updated_user.username])}
+
